90d1a0d618bc100ef80adc182d6ad2e302cd4cd4
PGM <qwe101801@naver.com>
2025-05-15 15:15:10 +0900

    first commit

---
+++ src/main/java/com/example/web_back/WebBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackApplication.class, args);
+    }
+
+}
